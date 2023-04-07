668a4074aca7d5379fecdfcdda1f4cd11143d65c
collinewait <collinewait17@gmail.com>
2023-04-07 13:40:01 +0300

    Toggle todo item

--- lib/todo_live_view_web/live/todos_list_component.ex
+++ lib/todo_live_view_web/live/todos_list_component.ex
@@ -1,3 +1,19 @@
 defmodule TodoLiveViewWeb.TodosListComponent do
   use TodoLiveViewWeb, :live_component
+  alias TodoLiveView.Todos
+
+  @todos_topic "todos"
+
+  def handle_event("toggle_todo", %{"id" => id}, socket) do
+    todo = Todos.get_todo!(id)
+    Todos.update_todo(todo, %{completed: !todo.completed})
+
+    socket = assign(socket, items: Todos.list_todos())
+    TodoLiveViewWeb.Endpoint.broadcast(@todos_topic, "todos_updated", socket.assigns)
+    {:noreply, socket}
+  end
+
+  def item_completed?(item) do
+    if item.completed == true, do: "completed", else: ""
+  end
 end
--- test/todo_live_view_web/live/todos_live_test.exs
+++ test/todo_live_view_web/live/todos_live_test.exs
@@ -1,6 +1,7 @@
 defmodule TodoLiveViewWeb.TodosLiveTest do
   use TodoLiveViewWeb.ConnCase
   import Phoenix.LiveViewTest
+  alias TodoLiveView.Todos
 
   @create_todo_attrs %{text: "first item"}
   @invalid_todo_attrs %{text: nil}
@@ -17,5 +18,16 @@ defmodule TodoLiveViewWeb.TodosLiveTest do
     |> render_submit()
 
     assert render(view) =~ "first item"
+  end
+
+  test "toggle todo item", %{conn: conn} do
+    {:ok, todo} = Todos.create_todo(%{"text" => "first item"})
+    assert todo.completed == false
+
+    {:ok, view, _html} = live(conn, "/")
+    assert view |> element("#item_completed") |> render_click() =~ "completed"
+
+    updated_todo = Todos.get_todo!(todo.id)
+    assert updated_todo.completed == true
   end
 end
